ce95a7637e56583e0e9bbcf5926edf67102f42db
Alexander Bartolomey <occloxium@gmail.com>
2019-10-20 16:52:05 +0200

    restructured logical flow of local build script

--- build-locally.sh
+++ build-locally.sh
@@ -2,14 +2,19 @@
 build () {
     echo "Please enter a tag for the image:"
     read TAG
-    docker build -t "docker-gurobi:$TAG" . 
-    echo "Image has been built."
+    docker build -t "docker-gurobi:$TAG" .
+    if [[ $? == 0 ]]; then
+        echo "Image has been built."
+    else
+        echo "An error occured while building docker image. Check log for details."
+        echo "Exiting without building image."
+    fi
 }
 
 echo "Currently on git branch $(git branch | grep \* | cut -d ' ' -f2)"
 echo "Do you want to build the docker-gurobi image from current branch? [y|N]"
 read CONFIRM
-if [[ $CONFIRM =~ ^[y|Y|j|J]$ ]]; then
+if [[ $CONFIRM =~ ^[y|Y|j|J]$ ]] || [[ $CONFIRM == "" ]]; then
     echo "Building image from branch $(git branch | grep \* | cut -d ' ' -f2)"
     build
 else
