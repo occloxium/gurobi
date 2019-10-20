a4d997a07d8a83d74f7eb867840d939562e00032
Alexander Bartolomey <occloxium@gmail.com>
2019-10-20 16:53:09 +0200

    refactored docker-entrypoint

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+solve () {
+    echo "Running solver on $1"
+    if [ -f $license ]; then
+        echo "Skipping license creation"
+        gurobi.sh $1
+    else
+        echo "Configure license $GUROBI_LICENSE"
+        echo -ne '\n' | grbgetkey $GUROBI_LICENSE
+        gurobi.sh $1
+    fi
+}
 
 set -e
 
@@ -14,16 +26,10 @@ echo "Mounted scripts available are:"
 ls -rAh
 echo
 if [ -z $MODEL ]; then
-    RUN=$MODEL
+    solve $MODEL
 else
-    RUN=(./*/*model.py)
-fi
-echo "Running solver on $RUN"
-if [ -f $license ]; then
-    echo "Skipping license creation"
-    gurobi.sh $RUN
-else
-    echo "Configure license $GUROBI_LICENSE"
-    echo -ne '\n' | grbgetkey $GUROBI_LICENSE
-    gurobi.sh $RUN
+    RUN=$(find . -name "*model.py")
+    find . -name "*model.py"|while read model; do 
+        solve $model
+    done;
 fi
